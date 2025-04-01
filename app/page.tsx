954f6e9aab6609663b9fb16f64b55443e1536f26
Brandon Butkovich <brandon.butk@gmail.com>
2025-04-01 15:45:31 -0700

    fix first name error

--- app/page.tsx
+++ app/page.tsx
@@ -291,7 +291,7 @@ export default function Home() {
                     value: (currentItem['Plan Description'].toLowerCase().includes('business') ? currentItem['Member Number'] : dupeArray.length !== 0 ? findPlan('Buisness', true) : ''),
                     inUse: false
                 },
-                'First Name FAIL HERE': {
+                'First Name': {
                     value: (nameFixer(currentItem['First Name'])), // FORCED ERROR HERE
                     inUse: false
                 },
